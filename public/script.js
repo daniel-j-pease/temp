534f0befdcf361d54c85053160b6b6efc9f68436
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-04 20:01:40 -0500

    first name draft

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log('script.js connected');
+window.onload = ( () =>{
+
+
+  const bar = document.getElementById('bar');
+  const name = document.getElementById('name');
+  const body = document.getElementsByTagName('body')
+
+  
+  window.addEventListener('resize', () => {
+    console.log('resize')
+  })
+})
