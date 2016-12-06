1a6f0ee266982c7a9aafe9b851b75b3fb72d56e1
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-06 00:14:01 -0500

    add phi to name positioning

--- public/script.js
+++ public/script.js
@@ -1,12 +1,35 @@
 window.onload = ( () =>{
 
+  const phi = 1.61803398875;
 
-  const bar = document.getElementById('bar');
-  const name = document.getElementById('name');
-  const body = document.getElementsByTagName('body')
+  const first = document.getElementById('first');
+  const last = document.getElementById('last');
+  const title = document.getElementById('title');
+  let pxStr = 'px'
 
-  
-  window.addEventListener('resize', () => {
-    console.log('resize')
-  })
+  const barOne = document.getElementById('bar-one');
+  const barTwo = document.getElementById('bar-two');
+  const barThree = document.getElementById('bar-three');
+  const barFour = document.getElementById('bar-four');
+
+  const firstLeft = first.getBoundingClientRect().left;
+  const lastRight = last.getBoundingClientRect().right;
+
+  console.log(firstLeft, lastRight)
+
+  first.style.right = window.innerWidth - (window.innerWidth*(1/phi)) + pxStr;
+  last.style.left = window.innerWidth - (window.innerWidth*(1/phi)) + pxStr;
+  first.style.bottom = window.innerHeight/phi + pxStr;
+  last.style.top = window.innerHeight - (window.innerHeight/phi) + pxStr;
+  title.style.top = window.innerHeight/phi + pxStr;
+
+  barOne.style.width = lastRight - firstLeft + pxStr;
+  barOne.style.right = first.style.right
+
+  function proportion() {
+    first.style.right = window.innerWidth - (window.innerWidth*(1/phi)) + pxStr;
+    last.style.left = window.innerWidth - (window.innerWidth*(1/phi)) + pxStr;
+  }
+
+  window.addEventListener('resize', proportion)
 })
